33451e66bb6941b70a93897db25f5358843dc5e8
Jonah-Northwood <north305@morris.umn.edu>
2020-09-22 10:47:12 -0500

    Initial setup of mergesort.c using java example
    Currently getting a stack overflow error so something likely isn't initialized correctly

--- mergesort/mergesort.h
+++ mergesort/mergesort.h
@@ -7,4 +7,11 @@
 
 void mergesort(int size, int values[]);
 
+void mergesortRange(int values[], int startIndex, int endIndex);
+
+bool needsSorting(int rangeSize);
+
+void mergeRanges(int values[], int startIndex, int midPoint,
+  int endIndex);
+
 #endif
